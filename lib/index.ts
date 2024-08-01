58c853a66c0a66ba0daa4a3c3760f34853875081
chriswoodcn <chriswoodcn@aliyun.com>
2024-08-01 15:38:25 +0800

    [update] 更新AES对称加解密方法

--- lib/index.ts
+++ lib/index.ts
@@ -4,15 +4,57 @@ import CryptoJS from 'crypto-js';
 
 export const withBasePath = (path: string) => path.startsWith("/") ? `${basePath}${path}` : `${basePath}/${path}`
 
-const AES_KEY = CryptoJS.enc.Utf8.parse(configuration.AppName);
-const AES_CipherOption = {
-  mode: CryptoJS.mode.ECB,
-  padding: CryptoJS.pad.Pkcs7
+const KEY_SIZE = 128;
+const fillKey = (key: string) => {
+  const filledKey = Buffer.alloc(KEY_SIZE / 8);
+  const keys = Buffer.from(key);
+  if (keys.length < filledKey.length) {
+    filledKey.map((b, i) => filledKey[i] = keys[i]);
+  }
+
+  return filledKey.toString();
 }
+const KEY_AES = CryptoJS.enc.Utf8.parse(fillKey(configuration.AppName));
+const aes_encrypt = (data: string, key: CryptoJS.lib.WordArray) => {
+  /**
+   * CipherOption, 加密的一些选项:
+   *   mode: 加密模式, 可取值(CBC, CFB, CTR, CTRGladman, OFB, ECB), 都在 CryptoJS.mode 对象下
+   *   padding: 填充方式, 可取值(Pkcs7, AnsiX923, Iso10126, Iso97971, ZeroPadding, NoPadding), 都在 CryptoJS.pad 对象下
+   *   iv: 偏移量, mode === ECB 时, 不需要 iv
+   * 返回的是一个加密对象
+   */
+  const cipher = CryptoJS.AES.encrypt(data, key, {
+    mode: CryptoJS.mode.ECB,
+    padding: CryptoJS.pad.Pkcs7,
+    iv: undefined,
+  });
+  // 将加密后的数据转换成 Base64
+  const base64Cipher = cipher.ciphertext.toString(CryptoJS.enc.Base64);
+  // 处理 Android 某些低版的BUG
+  const resultCipher = base64Cipher.replace(/\+/g, '-').replace(/\//g, '_');
+  // 返回加密后的经过处理的 Base64
+  return resultCipher;
+}
+const aes_decrypt = (encrypted: string, key: CryptoJS.lib.WordArray) => {
+  // 先将 Base64 还原一下, 因为加密的时候做了一些字符的替换
+  const restoreBase64 = encrypted.replace(/\-/g, '+').replace(/_/g, '/');
+  // 这里 mode, padding, iv 一定要跟加密的时候完全一样
+  // 返回的是一个解密后的对象
+  const decipher = CryptoJS.AES.decrypt(restoreBase64, key, {
+    mode: CryptoJS.mode.ECB,
+    padding: CryptoJS.pad.Pkcs7,
+    iv: undefined,
+  });
+  // 将解密对象转换成 UTF8 的字符串
+  const resultDecipher = CryptoJS.enc.Utf8.stringify(decipher);
+  // 返回解密结果
+  return resultDecipher;
+}
+
+
 export const aesEncrypt = (data: string) => {
-  return CryptoJS.AES.encrypt(data, AES_KEY, AES_CipherOption).toString();
+  return aes_encrypt(data, KEY_AES)
 }
 export const aesDecrypt = (encrypted: string) => {
-  const bytes = CryptoJS.AES.decrypt(encrypted, AES_KEY, AES_CipherOption);
-  return bytes.toString(CryptoJS.enc.Utf8);
+  return aes_decrypt(encrypted, KEY_AES)
 }
