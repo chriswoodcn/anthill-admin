cde6f3fb4142f015c324187c93a47538d63d2a19
chriswoodcn <chriswoodcn@aliyun.com>
2024-08-20 08:56:07 +0800

    [update] 增加引入css注释

--- app/template/forms/select2/ComponentsFormsSelectBasic.tsx
+++ app/template/forms/select2/ComponentsFormsSelectBasic.tsx
@@ -1,19 +1,19 @@
-'use client';
+"use client";
 
-import Select from 'react-select';
-import React from 'react';
-import PanelCodeHighlight from '@/components/compose/PanelCodeHighlight';
+import Select from "react-select";
+import React from "react";
+import PanelCodeHighlight from "@/components/compose/PanelCodeHighlight";
 
 const ComponentsFormsSelectBasic = () => {
-    const options = [
-        { value: 'orange', label: 'Orange' },
-        { value: 'white', label: 'White' },
-        { value: 'purple', label: 'Purple' },
-    ];
-    return (
-        <PanelCodeHighlight
-            title="Basic"
-            codeHighlight={`import Select from 'react-select';
+  const options = [
+    { value: "orange", label: "Orange" },
+    { value: "white", label: "White" },
+    { value: "purple", label: "Purple" },
+  ];
+  return (
+    <PanelCodeHighlight
+      title="Basic"
+      codeHighlight={`import Select from 'react-select';
 
 const options = [
     { value: 'orange', label: 'Orange' },
@@ -22,12 +22,16 @@ const options = [
 ];
 
 <Select defaultValue={options[0]} options={options} isSearchable={false} />`}
-        >
-            <div className="mb-5">
-                <Select defaultValue={options[0]} options={options} isSearchable={false} />
-            </div>
-        </PanelCodeHighlight>
-    );
+    >
+      <div className="mb-5">
+        <Select
+          defaultValue={options[0]}
+          options={options}
+          isSearchable={false}
+        />
+      </div>
+    </PanelCodeHighlight>
+  );
 };
 
 export default ComponentsFormsSelectBasic;
