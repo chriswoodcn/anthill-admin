d54268ebdafaed05e5885ca17d2e8a2dfd53e345
chriswoodcn <chriswoodcn@aliyun.com>
2024-09-13 11:14:20 +0800

    [update] 首页欢迎语国际化-服务端

--- app/admin/(layout)/index/page.tsx
+++ app/admin/(layout)/index/page.tsx
@@ -1,48 +1,14 @@
-"use client";
-
-import { withBasePath } from "@/lib";
-import { nextFetcher } from "@/lib/fetcher";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import useSWR from "swr";
 import configuraton from "@/configuration.mjs";
-import Toast from '@/lib/toast';
+import { getServerTranslations } from "@/i18n/server";
 
-export default () => {
-  const router = useRouter();
-  const [logoutAction, setLogoutAction] = useState(false);
-  const { data, error, isLoading } = useSWR(
-    logoutAction
-      ? {
-          url: withBasePath("/api/auth/logout"),
-          method: "GET",
-        }
-      : null,
-    nextFetcher,
-    {
-      onSuccess: (data) => {
-        if (data.code == 200) {
-          router.replace(configuraton.PathAlias.Admin.Login);
-        }
-      },
-      onError: (err) => {
-        Toast.fireErrorAction({
-          html: (
-            <p className="text-2xl font-bold">
-              {err.message || t("fetch_error")}
-            </p>
-          ),
-          timer: 0,
-        });
-      },
-    }
-  );
+export default async function () {
+  const { t } = await getServerTranslations();
   return (
     <div className="w-full h-[calc(100vh-10rem)] bg-bg_home bg-center rounded-lg relative">
       <div className="hidden dark:block absolute left-0 right-0 top-0 bottom-0 bg-black/50 z-[2] rounded-lg"></div>
       <div className="w-full h-full flex flex-col items-center justify-center relative z-10">
         <div className="text-white dark:text-white-7 text-5xl font-semibold leading-normal">
-          欢迎使用Anthill Admin后管
+          {t("admin-home-welcome")}
         </div>
         <div className="text-white dark:text-white-7 text-xl leading-normal">
           VERSION: {configuraton.Version}
@@ -53,4 +19,4 @@ export default () => {
       </div>
     </div>
   );
-};
+}
